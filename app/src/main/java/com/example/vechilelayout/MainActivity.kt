5c4ed9f8f6cf27270928cc827bc9126b2dcb0ccb
Kajal <kajalthadhani18@gmail.com>
2022-04-11 14:24:37 +0530

    adding activity

--- app/src/main/java/com/example/vechilelayout/MainActivity.kt
+++ app/src/main/java/com/example/vechilelayout/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.vechilelayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_looking_for.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bike_details)
+        setContentView(R.layout.activity_looking_for)
+
+        tvManageBike.setOnClickListener {
+            startActivity(Intent(this,VehicleListActivity::class.java))
+        }
+
     }
 }
