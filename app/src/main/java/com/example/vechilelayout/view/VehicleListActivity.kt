fa3b0ea1ba944cf9eda575b3aba4b526cc1ae66a
Kajal <kajalthadhani18@gmail.com>
2022-04-12 16:39:07 +0530

    added selection for filter_screen.xml

--- app/src/main/java/com/example/vechilelayout/view/VehicleListActivity.kt
+++ app/src/main/java/com/example/vechilelayout/view/VehicleListActivity.kt
@@ -7,16 +7,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vechilelayout.R
-import com.example.vechilelayout.adapter.*
+import com.example.vechilelayout.adapter.FilterCategoryBranddapter
+import com.example.vechilelayout.adapter.FilterCategoryTypedapter
+import com.example.vechilelayout.adapter.SpecificationAdapter
+import com.example.vechilelayout.adapter.VehicleListAdapter
 import com.example.vechilelayout.model.VehicleListModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.activity_bike_details.*
 import kotlinx.android.synthetic.main.activity_vehicle_list.*
 import kotlinx.android.synthetic.main.bottom_sheet_sortby.view.*
 import kotlinx.android.synthetic.main.custom_toolbarfilter.*
-import kotlinx.android.synthetic.main.dialog_specification.*
 import kotlinx.android.synthetic.main.filter_screen.*
-import kotlinx.android.synthetic.main.filter_screen.rvType
 
 class VehicleListActivity : AppCompatActivity() {
 
@@ -24,8 +24,10 @@ class VehicleListActivity : AppCompatActivity() {
     lateinit var adapter1 : FilterCategoryTypedapter
     lateinit var brandAdapter: FilterCategoryBranddapter
     lateinit var sheetBehavior: BottomSheetBehavior<*>
+    lateinit var specificationAdapter: SpecificationAdapter
 
     var vehicleList = ArrayList<VehicleListModel>()
+    var filterCtegoryList = ArrayList<VehicleListModel>()
     var categoryType = ArrayList<VehicleListModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,67 +55,74 @@ class VehicleListActivity : AppCompatActivity() {
             sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
         }
 
+        fun specificationItemClick(position : Int, dialog: Dialog){
+            categoryType.clear()
+            when(position){
+                1->{
+                    dialog.rvType.layoutManager = GridLayoutManager(this,2, RecyclerView.VERTICAL,false)
+                    categoryType.clear()
+                    categoryType.add(VehicleListModel("Hero"))
+                    categoryType.add(VehicleListModel("Suzuki"))
+                    categoryType.add(VehicleListModel("Bajaj"))
+                    categoryType.add(VehicleListModel("KTM"))
+                    categoryType.add(VehicleListModel("Royal Enfield"))
+                    categoryType.add(VehicleListModel("Yamaha"))
+                    categoryType.add(VehicleListModel("TVS Electric"))
+                    brandAdapter = FilterCategoryBranddapter(this,categoryType)
+                    dialog.rvType.adapter = brandAdapter
+                }
+                2->{
+                    dialog.rvType.layoutManager = GridLayoutManager(this,2, RecyclerView.VERTICAL,false)
+                    categoryType.clear()
+                    categoryType.add(VehicleListModel(">60k"))
+                    categoryType.add(VehicleListModel("60k - 80k"))
+                    categoryType.add(VehicleListModel("80k - 1L"))
+                    categoryType.add(VehicleListModel("1L - 2L"))
+                    categoryType.add(VehicleListModel("1.2L - 1.5L"))
+                    categoryType.add(VehicleListModel("1.5L - 1.7L"))
+                    categoryType.add(VehicleListModel("1.7L - 2L"))
+                    brandAdapter = FilterCategoryBranddapter(this,categoryType)
+                    dialog.rvType.adapter = brandAdapter
+                }
+                3->{
+                    dialog.rvType.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+                    categoryType.clear()
+                    categoryType.add(VehicleListModel("Below 40 Kmpl"))
+                    categoryType.add(VehicleListModel("40 - 60 Kmpl"))
+                    categoryType.add(VehicleListModel("60 - 80 Kmpl"))
+                    categoryType.add(VehicleListModel("Above 80 Kmpl"))
+                    brandAdapter = FilterCategoryBranddapter(this,categoryType)
+                    dialog.rvType.adapter = brandAdapter
+                }
+                else -> {
+                    dialog.rvType.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+                    categoryType.clear()
+                    categoryType.add(VehicleListModel("Moped"))
+                    categoryType.add(VehicleListModel("Bike"))
+                    categoryType.add(VehicleListModel("Scooty"))
+                    categoryType.add(VehicleListModel("Electric"))
+                    adapter1 = FilterCategoryTypedapter(this,categoryType)
+                    dialog.rvType.adapter = adapter1
+
+                }
+            }
+        }
+
         llFilter.setOnClickListener {
             val dialog = Dialog(this, R.style.Theme_AppCompat_Light_NoActionBar_FullScreen)
             dialog.setContentView(R.layout.filter_screen)
             dialog.location_icon.setOnClickListener {
                 dialog.dismiss()
             }
-            dialog.rvType.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
-            categoryType.clear()
-            categoryType.add(VehicleListModel("Moped"))
-            categoryType.add(VehicleListModel("Bike"))
-            categoryType.add(VehicleListModel("Scooty"))
-            categoryType.add(VehicleListModel("Electric"))
-            adapter1 = FilterCategoryTypedapter(this,categoryType)
-            dialog.rvType.adapter = adapter1
-
-            dialog.llType.setOnClickListener {
-                dialog.rvType.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
-                categoryType.clear()
-                categoryType.add(VehicleListModel("Moped"))
-                categoryType.add(VehicleListModel("Bike"))
-                categoryType.add(VehicleListModel("Scooty"))
-                categoryType.add(VehicleListModel("Electric"))
-                adapter1 = FilterCategoryTypedapter(this,categoryType)
-                dialog.rvType.adapter = adapter1
-            }
-            dialog.llBrand.setOnClickListener {
-                dialog.rvType.layoutManager = GridLayoutManager(this,2, RecyclerView.VERTICAL,false)
-                categoryType.clear()
-                categoryType.add(VehicleListModel("Hero"))
-                categoryType.add(VehicleListModel("Suzuki"))
-                categoryType.add(VehicleListModel("Bajaj"))
-                categoryType.add(VehicleListModel("KTM"))
-                categoryType.add(VehicleListModel("Royal Enfield"))
-                categoryType.add(VehicleListModel("Yamaha"))
-                categoryType.add(VehicleListModel("TVS Electric"))
-                brandAdapter = FilterCategoryBranddapter(this,categoryType)
-                dialog.rvType.adapter = brandAdapter
-            }
-            dialog.llBudget.setOnClickListener {
-                dialog.rvType.layoutManager = GridLayoutManager(this,2, RecyclerView.VERTICAL,false)
-                categoryType.clear()
-                categoryType.add(VehicleListModel(">60k"))
-                categoryType.add(VehicleListModel("60k - 80k"))
-                categoryType.add(VehicleListModel("80k - 1L"))
-                categoryType.add(VehicleListModel("1L - 2L"))
-                categoryType.add(VehicleListModel("1.2L - 1.5L"))
-                categoryType.add(VehicleListModel("1.5L - 1.7L"))
-                categoryType.add(VehicleListModel("1.7L - 2L"))
-                brandAdapter = FilterCategoryBranddapter(this,categoryType)
-                dialog.rvType.adapter = brandAdapter
-            }
-            dialog.llMileage.setOnClickListener {
-                dialog.rvType.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
-                categoryType.clear()
-                categoryType.add(VehicleListModel("Below 40 Kmpl"))
-                categoryType.add(VehicleListModel("40 - 60 Kmpl"))
-                categoryType.add(VehicleListModel("60 - 80 Kmpl"))
-                categoryType.add(VehicleListModel("Above 80 Kmpl"))
-                brandAdapter = FilterCategoryBranddapter(this,categoryType)
-                dialog.rvType.adapter = brandAdapter
-            }
+            specificationItemClick(0,dialog)
+            dialog.rvFiterCategory.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+            filterCtegoryList.clear()
+            filterCtegoryList.add(VehicleListModel("Engine",R.drawable.ic_engine))
+            filterCtegoryList.add(VehicleListModel("Body Dimensions",R.drawable.ic_body_dimension))
+            filterCtegoryList.add(VehicleListModel("Chasis & Suspensions",R.drawable.ic_suspensions))
+            filterCtegoryList.add(VehicleListModel("Types & Brakes",R.drawable.ic_tyres))
+            specificationAdapter = SpecificationAdapter(this,filterCtegoryList,{position : Int -> specificationItemClick(position,dialog) })
+            dialog.rvFiterCategory.adapter = specificationAdapter
             dialog.show()
         }
     }
