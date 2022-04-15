925afd616699fe5054c150555f9d2135cfc833ce
Kajal <kajalthadhani18@gmail.com>
2022-04-15 09:48:43 +0530

    change in loan UI

--- app/src/main/java/com/example/vechilelayout/view/MainActivity.kt
+++ app/src/main/java/com/example/vechilelayout/view/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.vechilelayout.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vechilelayout.R
 import kotlinx.android.synthetic.main.activity_looking_for.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loan_info)
+        setContentView(R.layout.activity_looking_for)
 
-      /*  tvManageBike.setOnClickListener {
+        tvApplyForLoan.setOnClickListener {
+            startActivity(Intent(this, ApplyLoanActivity::class.java))
+        }
+
+        tvManageBike.setOnClickListener {
             startActivity(Intent(this, VehicleListActivity::class.java))
-        }*/
+        }
 
     }
 }
